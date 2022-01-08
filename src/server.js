f22e71784dd07affbd98b5b144b3fdf8c1e3b266
Junior Paula <slucivaldo509@gmail.com>
2022-01-08 09:11:36 -0300

    configurate dotenv

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import app from './app';
 
-app.listen(3003, () => {
-  console.log('server is running');
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log('-------(^-^)-------');
+  console.log(`Server started on port ${port}`);
 });
