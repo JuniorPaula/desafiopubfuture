26cc1819913d6cae28cac1a93622d86b1ed39d01
Junior Paula <slucivaldo509@gmail.com>
2022-01-08 13:34:47 -0300

    create method to show all balances sum

--- src/controllers/AccountsController.js
+++ src/controllers/AccountsController.js
@@ -61,12 +61,58 @@ class AccountsController {
 
       const newAccount = await Account.findByPk(req.params.id);
 
+      if (!newAccount) {
+        return res.status(400).json({
+          errors: ['Account not found.'],
+        });
+      }
+
       const account = await newAccount.update({
         type_account,
         financial_institution,
       });
 
       return res.status(200).json(account);
+    } catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map((e) => e.message),
+      });
+    }
+  }
+
+  /** método responsável por deletar uma conta */
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      const account = await Account.findByPk(id);
+
+      if (!account) {
+        return res.status(400).json({
+          errors: ['Account not found.'],
+        });
+      }
+
+      await account.destroy(id);
+
+      return res.status(200).json([]);
+    } catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map((e) => e.message),
+      });
+    }
+  }
+
+  /** método responsável por listar saldo total */
+  async getTotalBalance(req, res) {
+    try {
+      const accounts = await Account.findAll();
+      const balances = accounts.map((value) => value.balance);
+      const total = balances.reduce((ac, el) => ac + el, 0);
+
+      return res.status(200).json({
+        saldo_total: total,
+      });
     } catch (err) {
       return res.status(400).json({
         errors: err.errors.map((e) => e.message),
--- src/routes/accountRoutes.js
+++ src/routes/accountRoutes.js
@@ -6,5 +6,8 @@ const router = Router();
 router.get('/', accountsController.index);
 router.post('/', accountsController.create);
 router.put('/:id', accountsController.update);
+router.delete('/:id', accountsController.delete);
+
+router.get('/saldos', accountsController.getTotalBalance);
 
 export default router;
