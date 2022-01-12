5961c9ac1f6ae85b0b4a47c0e80f66189da8c3ca
Junior Paula <slucivaldo509@gmail.com>
2022-01-12 19:46:25 -0300

    create migration of the costs table

---
+++ src/database/migrations/20220112224228-costs.js
@@ -0,0 +1,48 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('costs', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      amount: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      receipt_date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      expected_receipt_date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      account_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'accounts',
+          key: 'id',
+        },
+      },
+      type_income: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('costs');
+  },
+};
